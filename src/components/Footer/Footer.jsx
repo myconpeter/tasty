63ac4cdac5a12ebb3374249c0608081e1410b615
Micheal Peter <79177019+myconpeter@users.noreply.github.com>
2024-09-05 07:21:21 +0100

    ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,42 @@
-import React from "react";
-import { ListGroup } from "reactstrap";
+import React from 'react';
+import { ListGroup } from 'reactstrap';
 
-import logo from "../../assets/images/res-logo.png";
-import "../../styles/footer.css";
+import logo from '../../assets/images/res-logo.png';
+import '../../styles/footer.css';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer__logo">
-        <img src={logo} alt="logo" />
-        <h5>MyPizza</h5>
-        <p>Best Pizzas in town, try it out!</p>
-      </div>
-      <div>
-        <h5 className="footer__title mb-3">Delivery Time</h5>
-        <ListGroup>
-          <div className="delivery__time-item border-0 ps-0">
-            <span>Friday - Tuesday</span>
-            <p>10:00am - 11:00pm</p>
-          </div>
-          <div className="delivery__time-item border-0 ps-0">
-            <span>Wednesday - Thursday</span>
-            <p>Off day</p>
-          </div>
-        </ListGroup>
-      </div>
-    </footer>
-  );
+	return (
+		<>
+			{' '}
+			<footer className='footer'>
+				<div className='footer__logo'>
+					<img
+						src={logo}
+						alt='logo'
+					/>
+					<h5>MyPizza</h5>
+					<p>Best Pizzas in town, try it out!</p>
+				</div>
+				<div>
+					<h5 className='footer__title mb-3'>Delivery Time</h5>
+					<ListGroup>
+						<div className='delivery__time-item border-0 ps-0'>
+							<span>Friday - Tuesday</span>
+							<p>10:00am - 11:00pm</p>
+						</div>
+						<div className='delivery__time-item border-0 ps-0'>
+							<span>Wednesday - Thursday</span>
+							<p>Off day</p>
+						</div>
+					</ListGroup>
+				</div>
+			</footer>
+			<div>
+				<p className='owner'>NGOZI, ALWELL UGONNA</p>
+				<p className='owner'>20/SC/CO/957</p>
+			</div>
+		</>
+	);
 };
 
 export default Footer;
